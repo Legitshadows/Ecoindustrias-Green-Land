c0c42eeb9b7c0d9c46708bdbae5d1439c0b3aca1
Legitshadows <la.beltran19@info.uas.edu.mx>
2022-12-30 05:40:46 -0700

    Got code snippet for contact page from old project

--- EcoGreenLand/src/app/app.module.ts
+++ EcoGreenLand/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,9 @@ import { ContactComponent } from './pages/contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- EcoGreenLand/src/app/pages/contact/contact.component.ts
+++ EcoGreenLand/src/app/pages/contact/contact.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
+
+
 export class ContactComponent {
 
-}
+  constructor() {}
+
+  contactoForm:any; //form 
+
+  ngOnInit(): void {
+    this.contactoForm = new FormGroup({
+      "nombres":new FormControl(null, [Validators.required,Validators.pattern('[a-zA-Z ]*')]),
+      "apellidos":new FormControl(null),
+      "correo":new FormControl(null),
+      "celular":new FormControl(null),
+      "mensaje":new FormControl(null),
+    })
+  }
+
+  // Enviar Funcion
+  enviarData() {
+    console.log(this.contactoForm.value);
+      alert('Gracias por su mensaje!')
+      this.contactoForm.reset();
+    }
+  }
+  
+
+
